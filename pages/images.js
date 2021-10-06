25db937d498c71883d55661847bcd378b3ca8406
sarisgar28 <sarisgar28@gmail.com>
2021-10-06 17:02:33 -0400

    progress

--- pages/images.js
+++ pages/images.js
@@ -1,24 +1,54 @@
+import Head from 'next/head'
+import {useRouter} from 'next/router'
+import DefaultErrorPage from 'next/error'
+
 
 export const getStaticProps = async() =>{
     const res = await fetch('https://jsonplaceholder.typicode.com/photos');
     const data = await res.json();
+    if(!data){
+        return{
+            notFound: true,
+        }
+    }
     return{
-      props: {images: data}
+      props: {
+        images: data
+      }
     }
 }
 
-const Images = ({images}) =>{
+
+
+function Images({images}){
+    const router = useRouter()
+    // show a loading state when the fallback is rendered.
+    if(router.isFallback){
+        return <h1>Loading...</h1>
+    }
+    // this includes setting the noindex header because static files alwaysreturn a status 200.
+    if(images.length === 0){
+        return 
+        <>
+        <Head>
+            <meta name="robots" content="noindex"/>
+        </Head>
+        <DefaultErrorPage statusCode={404}/>
+     
+        </>
+    }
+    // defaultErrorPage creates a 404 error 
     return (
-        <div>
+        <ul>
             <h1>Images</h1>
             {images.map(image =>(
               <div key={image.id}> 
                    <li>{image.title}</li>
-                <img src={image.url}/>
-
-              </div>
+                    <img src={image.url}/>
+             </div>
            ))}
-        </div>
+        </ul>
+       
     )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
+      <Head>
       <h1>Home</h1>
+      </Head>
+     
       <li>
         <Link href="/">
           <a>HOME</a>
